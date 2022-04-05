138b28774a86614041697e3bf885388c84ba61ac
Jasper10636 <49470734+Jasper10636@users.noreply.github.com>
2022-04-05 18:56:08 -0500

    React config

---
+++ src/main/resources/static/precache-manifest.aed58c33eb23407f5d955e28ea5fc321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b0a8d778365a63f071bba0d33c6805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "02f946cd5a97bf0f9d07",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "02f946cd5a97bf0f9d07",
+    "url": "/static/js/main.38489c45.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
